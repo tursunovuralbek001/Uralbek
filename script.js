c53b1303c4c1d803e64459ff5f51dbf2e0593d98
tursunovuralbek001 <tursunovuralbek001@gmail.com>
2025-01-05 21:20:58 +0500

    update script.js

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function ochish() {
 function yopish() {
   document.getElementById("mySidebar").style.display = "none";
 }
+
+//Clock
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  h = checkTime(h);
+  m = checkTime(m);
+  s = checkTime(s);
+  
+document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
